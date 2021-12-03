4e26eafd4a2b85c47f69a6210c7f920771a3c2dd
Takeshido-ux <isma555555655@gmail.com>
2021-12-03 16:38:05 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -2,15 +2,34 @@ let inptName = $('#GetName');
 let inptInventory = $('#GetInventory');
 let inptVariants = $('#GetVariants');
 let inptPrice = $('#GetPrices').mask('09999.99');
-let orderDate = new Date($.now()).toLocaleDateString();
+let dateAdd, dateChange;
+let id = 0;
 let Order = {
     name: inptName.val(),
     inventory: inptInventory.val(),
     variants: inptVariants.val(),
     price: inptPrice.val()
 };
+let arrayOrder = [];
+
+
 
 $('#addOrder').click(function(){
+    dateAdd = new Date().toLocaleString();
+    let obj = {
+        id: id,
+        name: inptName.val(),
+        inventory: inptInventory.val(),
+        variants: inptVariants.val(),
+        price: inptPrice.val(),
+        dateAdd: dateAdd,
+        dateChange: dateChange ?? ''
+    }
+    arrayOrder.push(obj);
+
+
+
+
     Order = {
         name: inptName.val(),
         inventory: inptInventory.val(),
@@ -22,16 +41,16 @@ $('#addOrder').click(function(){
     inptVariants.val('');
     inptPrice.val('');
 
-
     if(Order.name && Order.inventory && Order.inventory && Order.price){
         $('#orderBox').append(
         `
-        <tr class="order" data-name="${Order.name}" data-inventory="${Order.inventory}" data-variants="${Order.variants}" data-price="${Order.price}" data-Date="${orderDate}">
-            <td id="name" class="align-middle info-order" colspan="2">${Order.name}</td>
+        <tr id="${id}" class="order" data-name="${Order.name}" data-inventory="${Order.inventory}" data-variants="${Order.variants}" data-price="${Order.price}" data-dateAdd="${dateAdd}" data-dateChange="${dateChange}">
+            <td id="name" class="align-middle info-order" colspan="1">${Order.name}</td>
             <td id="inventory" class="align-middle info-order">${Order.inventory}</td>
             <td id="variants" class="align-middle info-order">${Order.variants}</td>
             <td id="price" class="align-middle info-order">${Order.price}</td>
-            <td class="align-middle info-order">${orderDate}</td>
+            <td class="align-middle info-order">${dateAdd}</td>
+            <td class="align-middle info-order">${dateChange ?? ''}</td>
             <td class="align-middle text-center">
             <a href="#" id="changeOrderTo" class="btn btn-sm btn-icon btn-secondary" data-toggle="sidebar">
                 <i class="fa fa-pencil-alt"></i> 
@@ -49,6 +68,7 @@ $('#addOrder').click(function(){
     else{
         alert('Введите все значения!')
     }
+    id = id + 1;
 })
 $('#addBtnTo').click(function(){
     $('#addOrder').removeClass('d-none');
@@ -73,11 +93,24 @@ $('#orderBox').on('click', '#changeOrderTo', function(){
     $('#addOrder').addClass('d-none');
 })
 $('#changeOrder').click(function(){
+    dateChange = new Date().toLocaleString();
+    arrayOrder.forEach(item => {
+        if(item.id == changeOrder.attr('id')){
+            item.name = inptName.val();
+            item.inventory = inptInventory.val();
+            item.variants = inptVariants.val();
+            item.price = inptPrice.val();
+            item.dateAdd = dateAdd;
+            item.dateChange = dateChange ?? '';
+        } 
+    })
+
+
     Order = {
         name: inptName.val(),
         inventory: inptInventory.val(),
         variants: inptVariants.val(),
-        price: inptPrice.val()
+        price: inptPrice.val(),
     }
     inptName.val('');
     inptInventory.val('');
@@ -85,13 +118,18 @@ $('#changeOrder').click(function(){
     inptPrice.val('');
 
     if(Order.name && Order.inventory && Order.inventory && Order.price){
+        changeOrder.attr('data-name', Order.name)
+        changeOrder.attr('data-inventory', Order.inventory)
+        changeOrder.attr('data-variants', Order.variants)
+        changeOrder.attr('data-price', Order.price)
         changeOrder.html(
             `
-                <td class="align-middle info-order" colspan="2">${Order.name}</td>
-                <td class="align-middle info-order">${Order.inventory}</td>
-                <td class="align-middle info-order">${Order.variants}</td>
-                <td class="align-middle info-order">${Order.price}</td>
-                <td class="align-middle info-order">${orderDate}</td>
+            <td id="name" class="align-middle info-order" colspan="1">${Order.name}</td>
+            <td id="inventory" class="align-middle info-order">${Order.inventory}</td>
+            <td id="variants" class="align-middle info-order">${Order.variants}</td>
+            <td id="price" class="align-middle info-order">${Order.price}</td>
+            <td class="align-middle info-order">${dateAdd}</td>
+            <td class="align-middle info-order">${dateChange}</td>
                 <td class="align-middle text-center">
                 <a href="#" id="changeOrderTo" class="btn btn-sm btn-icon btn-secondary" data-toggle="sidebar">
                     <i class="fa fa-pencil-alt"></i> 
@@ -218,12 +256,17 @@ $('#sortBtnPrs').click(function(){
 })
 
 
-$('#sortBtnDate').click(function(){
+$('#sortBtnDateAdd').click(function(){
     let orderBox = document.querySelector('#orderBox');
     if($(this).hasClass('sorting_asc')){
         for(let i = 0; i < orderBox.children.length; i++){
             for(let j = i; j < orderBox.children.length; j++){
-                if(+orderBox.children[i].getAttribute('data-Date') > +$('.order')[j].getAttribute('data-Date')){
+                let datePrev = orderBox.children[i].getAttribute('data-dateAdd');
+                let dateNext = orderBox.children[j].getAttribute('data-dateAdd');
+                datePrev = Date.parse(datePrev);
+                dateNext = Date.parse(dateNext);
+                console.log(datePrev,dateNext)
+                if( datePrev > dateNext){
                     replacedNode = orderBox.replaceChild(orderBox.children[j], orderBox.children[i]);
                     insertAfter(replacedNode, orderBox.children[i])
                 }
@@ -233,7 +276,42 @@ $('#sortBtnDate').click(function(){
     else if($(this).hasClass('sorting_desc')){
         for(let i = 0; i < orderBox.children.length; i++){
             for(let j = i; j < orderBox.children.length; j++){
-                if(+orderBox.children[i].getAttribute('data-Date') < +$('.order')[j].getAttribute('data-Date')){
+                let datePrev = orderBox.children[i].getAttribute('data-dateAdd');
+                let dateNext = orderBox.children[j].getAttribute('data-dateAdd');
+                datePrev = Date.parse(datePrev);
+                dateNext = Date.parse(dateNext);
+                if( datePrev < dateNext){
+                    replacedNode = orderBox.replaceChild(orderBox.children[j], orderBox.children[i]);
+                    insertAfter(replacedNode, orderBox.children[i])
+                }
+            }
+        }
+    }
+})
+$('#sortBtnDateChange').click(function(){
+    let orderBox = document.querySelector('#orderBox');
+    if($(this).hasClass('sorting_asc')){
+        for(let i = 0; i < orderBox.children.length; i++){
+            for(let j = i; j < orderBox.children.length; j++){
+                let datePrev = orderBox.children[i].getAttribute('data-dateChange');
+                let dateNext = orderBox.children[j].getAttribute('data-dateChange');
+                datePrev = Date.parse(datePrev);
+                dateNext = Date.parse(dateNext);
+                if( datePrev > dateNext){
+                    replacedNode = orderBox.replaceChild(orderBox.children[j], orderBox.children[i]);
+                    insertAfter(replacedNode, orderBox.children[i])
+                }
+            }
+        }
+    }
+    else if($(this).hasClass('sorting_desc')){
+        for(let i = 0; i < orderBox.children.length; i++){
+            for(let j = i; j < orderBox.children.length; j++){
+                let datePrev = orderBox.children[i].getAttribute('data-dateChange');
+                let dateNext = orderBox.children[j].getAttribute('data-dateChange');
+                datePrev = Date.parse(datePrev);
+                dateNext = Date.parse(dateNext);
+                if( datePrev < dateNext){
                     replacedNode = orderBox.replaceChild(orderBox.children[j], orderBox.children[i]);
                     insertAfter(replacedNode, orderBox.children[i])
                 }
@@ -258,7 +336,23 @@ function insertAfter(newNode, referenceNode) {
 
 
 
-
+$('#showOrder').click(function(){
+    $('#arrayOrder').html('')
+    arrayOrder.forEach(item => {
+        $('#arrayOrder').append(
+        `
+        <tr id="${item.id}" class="order" data-name="${item.name}" data-inventory="${item.inventory}" data-variants="${item.variants}" data-price="${item.price}" data-dateAdd="${item.dateAdd}" data-dateChange="${item.dateChange}">
+            <td id="name" class="align-middle info-order" colspan="1"> name ${item.name}</td>
+            <td id="inventory" class="align-middle info-order"> inventory ${item.inventory}</td>
+            <td id="variants" class="align-middle info-order"> variants ${item.variants}</td>
+            <td id="price" class="align-middle info-order"> price ${item.price}</td>
+            <td class="align-middle info-order"> dateAdd ${item.dateAdd}</td>
+            <td class="align-middle info-order"> dateChange ${item.dateChange ?? ''}</td>
+        </tr>
+        `
+        )
+    })
+})
 
 
 
